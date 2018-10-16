8ec0c2fc43a421ccdc1a6d58bd87ba2e486d2120
JinGyeong Jeong <jjg@kodebox.io>
2018-10-16 13:32:18 +0900

    Add tests for AssetTransferAddress

--- test/address.test.ts
+++ test/address.test.ts
@@ -1,6 +1,11 @@
 import { AssetTransferAddress, PlatformAddress } from "..";
 
 describe("AssetTransferAddress", () => {
+    const validAddressString = "ccaqyqjmvml2hdp8s8xzqnr57r8ywtduy2u6gcq89vffl";
+    const validAddress = AssetTransferAddress.fromString(validAddressString);
+    // Invalid length
+    const invalidAddressString = "ccaqyqjmvml2hdp8s8xzqnr57r8ywtduy2u6gcq89vff";
+
     test("import", () => {
         expect(typeof AssetTransferAddress).toBe("function");
     });
@@ -10,11 +15,64 @@ describe("AssetTransferAddress", () => {
         expect(typeof obj.AssetTransferAddress).toBe("function");
     });
 
-    test.skip("check", done => done.fail("not implemented"));
-    test.skip("ensure", done => done.fail("not implemented"));
-    test.skip("fromTypeAndPayload", done => done.fail("not implemented"));
-    test.skip("fromString", done => done.fail("not implemented"));
-    test.skip("toString", done => done.fail("not implemented"));
+    test("check", () => {
+        expect(AssetTransferAddress.check(validAddressString)).toBe(true);
+        expect(AssetTransferAddress.check(validAddress)).toBe(true);
+        expect(AssetTransferAddress.check(invalidAddressString)).toBe(false);
+    });
+
+    test("ensure", () => {
+        expect(AssetTransferAddress.ensure(validAddress)).toBe(validAddress);
+        expect(AssetTransferAddress.ensure(validAddressString)).toEqual(
+            validAddress
+        );
+    });
+
+    test.each([
+        [0, "0000000000000000000000000000000000000000", "tc", 1, false],
+        [1, "0000000000000000000000000000000000000000", "tc", 1, false],
+        [2, "0000000000000000000000000000000000000000", "tc", 1, false],
+        [0, "0000000000000000000000000000000000000000", "tc", 0, true],
+        [-1, "0000000000000000000000000000000000000000", "tc", 1, true],
+        [3, "0000000000000000000000000000000000000000", "tc", 1, true]
+    ])(
+        "fromTypeAndPayload type = %p payload = %p networkId = %p version = %p",
+        (type, payload, networkId, version, shouldThrow) => {
+            if (shouldThrow) {
+                expect(() => {
+                    AssetTransferAddress.fromTypeAndPayload(type, payload, {
+                        networkId,
+                        version
+                    });
+                }).toThrow();
+            } else {
+                expect(() => {
+                    AssetTransferAddress.fromTypeAndPayload(type, payload, {
+                        networkId,
+                        version
+                    });
+                }).not.toThrow();
+            }
+        }
+    );
+
+    test("fromString", () => {
+        expect(AssetTransferAddress.fromString(validAddressString)).toEqual(
+            validAddress
+        );
+
+        expect(() => {
+            AssetTransferAddress.fromString(
+                "cccqyqjmvml2hdp8s8xzqnr57r8ywtduy2u6gcq89vff"
+            );
+        }).toThrow();
+
+        // FIXME: wrong version, wrong type
+    });
+
+    test("toString", () => {
+        expect(validAddress.toString()).toEqual(validAddressString);
+    });
 });
 
 describe("PlatformAddress", () => {
