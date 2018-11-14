60df25593e60fefd9b71728e99b98aa6a83bbca8
JinGyeong Jeong <jjg@kodebox.io>
2018-11-14 20:02:04 +0900

    Refactor Hxxx.test.ts by using describe.each()

--- test/Hxxx.test.ts
+++ test/Hxxx.test.ts
@@ -1,79 +1,36 @@
+import * as _ from "lodash";
+
 import { H128, H160, H256, H512 } from "..";
 
-describe("H128", () => {
+describe.each([
+    [H128, "H128", 16],
+    [H160, "H160", 20],
+    [H256, "H256", 32],
+    [H512, "H512", 64]
+])("%p", (Hxxx, className, byteLength) => {
     test("import", () => {
-        expect(typeof H128).toBe("function");
+        expect(typeof Hxxx).toBe("function");
     });
 
     test("require", () => {
         const obj = require("..");
-        expect(typeof obj.H128).toBe("function");
+        expect(typeof obj[className]).toBe("function");
     });
 
     test("check", () => {
-        const obj = new H128("00000000000000000000000000000000");
-        expect(H128.check(obj)).toBe(true);
-        expect(H128.check("00000000000000000000000000000000")).toBe(true);
-        expect(H128.check("0000000000000000000000000000000g")).toBe(false);
-        expect(H128.check("0000000000000000000000000000000")).toBe(false);
-        expect(H128.check("000000000000000000000000000000000")).toBe(false);
+        const zero = _.repeat("00", byteLength);
+        expect(Hxxx.check(new Hxxx(zero))).toBe(true);
+        expect(Hxxx.check(zero)).toBe(true);
+        expect(Hxxx.check(zero.substr(1) + "F")).toBe(true);
+        expect(Hxxx.check(zero.substr(1) + "f")).toBe(true);
+        expect(Hxxx.check(zero.substr(1) + "g")).toBe(false);
+        expect(Hxxx.check(zero.substr(1) + "g")).toBe(false);
+        expect(Hxxx.check(zero + "0")).toBe(false);
     });
 
     test.skip("ensure", done => done.fail("not implemented"));
     test.skip("fromBytes", done => done.fail("not implemented"));
     test.skip("isEqualTo", done => done.fail("not implemented"));
-    test.skip("rlpBytes", done => done.fail("not implemented"));
-    test.skip("toEncodeObject", done => done.fail("not implemented"));
-});
-
-describe("H160", () => {
-    test("import", () => {
-        expect(typeof H160).toBe("function");
-    });
-
-    test("require", () => {
-        const obj = require("..");
-        expect(typeof obj.H160).toBe("function");
-    });
-
-    test.skip("check", done => done.fail("not implemented"));
-    test.skip("ensure", done => done.fail("not implemented"));
-    test.skip("fromBytes", done => done.fail("not implemented"));
-    test.skip("rlpBytes", done => done.fail("not implemented"));
-    test.skip("toEncodeObject", done => done.fail("not implemented"));
-});
-
-describe("H256", () => {
-    test("import", () => {
-        expect(typeof H256).toBe("function");
-    });
-
-    test("require", () => {
-        const obj = require("..");
-        expect(typeof obj.H256).toBe("function");
-    });
-
-    test.skip("check", done => done.fail("not implemented"));
-    test.skip("ensure", done => done.fail("not implemented"));
-    test.skip("fromBytes", done => done.fail("not implemented"));
-    test.skip("isEqualTo", done => done.fail("not implemented"));
-    test.skip("rlpBytes", done => done.fail("not implemented"));
-    test.skip("toEncodeObject", done => done.fail("not implemented"));
-});
-
-describe("H512", () => {
-    test("import", () => {
-        expect(typeof H512).toBe("function");
-    });
-
-    test("require", () => {
-        const obj = require("..");
-        expect(typeof obj.H512).toBe("function");
-    });
-
-    test.skip("check", done => done.fail("not implemented"));
-    test.skip("ensure", done => done.fail("not implemented"));
-    test.skip("fromBytes", done => done.fail("not implemented"));
     test.skip("rlpBytes", done => done.fail("not implemented"));
     test.skip("toEncodeObject", done => done.fail("not implemented"));
 });
