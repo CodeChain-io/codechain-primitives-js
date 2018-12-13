32e906733bb09ba76dd142de7e7ed2d59b84a0d4
JinGyeong Jeong <jjg@kodebox.io>
2018-12-13 16:31:59 +0900

    Change EcdsaSignature type into string

--- src/key/ecdsa.ts
+++ src/key/ecdsa.ts
@@ -1,3 +1,5 @@
+import * as _ from "lodash";
+
 /**
  * @hidden
  */
@@ -7,17 +9,13 @@ const EC = require("elliptic").ec;
  */
 const secp256k1 = new EC("secp256k1");
 
-export interface EcdsaSignature {
-    r: string;
-    s: string;
-    v: number;
-}
+export type EcdsaSignature = string;
 
 /**
  * Gets ECDSA signature for message from private key.
  * @param message 32 byte hexademical string
  * @param priv 32 byte hexadecimal string of private key
- * @returns r, s, v of ECDSA signature
+ * @returns 65 byte hexadecimal string of ECDSA signature
  */
 export const signEcdsa = (message: string, priv: string): EcdsaSignature => {
     if (!/^[0-9a-fA-F]{64}$/.test(message)) {
@@ -28,17 +26,16 @@ export const signEcdsa = (message: string, priv: string): EcdsaSignature => {
     }
     const key = secp256k1.keyFromPrivate(priv);
     const { r, s, recoveryParam: v } = key.sign(message, { canonical: true });
-    return {
-        r: r.toString("hex"),
-        s: s.toString("hex"),
-        v
-    };
+    const paddedR = _.padStart(r.toString(16), 64, "0");
+    const paddedS = _.padStart(s.toString(16), 64, "0");
+    const paddedV = _.padStart(v.toString(16), 2, "0");
+    return `${paddedR}${paddedS}${paddedV}`;
 };
 
 /**
  * Checks if the signature from signEcdsa is correct.
  * @param message 32 byte hexademical string
- * @param signature r, s, v of ECDSA signature
+ * @param signature 65 byte hexadecimal string of ECDSA signature
  * @param pub 64 byte hexadecimal string of public key
  * @returns if signature is valid, true. Else false.
  */
@@ -50,25 +47,29 @@ export const verifyEcdsa = (
     if (!/^[0-9a-fA-F]{64}$/.test(message)) {
         throw new Error(`invalid message: ${message}`);
     }
-    if (
-        !/^[0-9a-fA-F]{1,64}$/.test(signature.r) ||
-        !/^[0-9a-fA-F]{1,64}$/.test(signature.s) ||
-        signature.v < 0 ||
-        signature.v > 3
-    ) {
+    if (!/^(0x)?[0-9a-fA-F]{130}$/.test(signature)) {
         throw new Error(`invalid signature: ${signature}`);
+    }
+    if (signature.startsWith("0x")) {
+        signature = signature.substr(2);
+    }
+    const r = `${signature.substr(0, 64)}`;
+    const s = `${signature.substr(64, 64)}`;
+    const v = Number.parseInt(signature.substr(128, 2), 16);
+    if (v < 0 || v > 3) {
+        throw new Error(`invalid recoveryParam v: ${v}`);
     }
     if (!/^[0-9a-fA-F]{128}$/.test(pub)) {
         throw new Error(`invalid public key: ${pub}`);
     }
     const key = secp256k1.keyFromPublic("04" + pub, "hex");
-    return key.verify(message, signature);
+    return key.verify(message, { r, s, v });
 };
 
 /**
  * Gets public key from the message and ECDSA signature.
  * @param message 32 byte hexademical string
- * @param signature r, s, v of ECDSA signature
+ * @param signature 65 byte hexadecimal string of ECDSA signature
  * @returns 64 byte hexadecimal string public key
  */
 export const recoverEcdsa = (
@@ -78,13 +79,17 @@ export const recoverEcdsa = (
     if (!/^[0-9a-fA-F]{64}$/.test(message)) {
         throw new Error(`invalid message: ${message}`);
     }
-    if (
-        !/^[0-9a-fA-F]{1,64}$/.test(signature.r) ||
-        !/^[0-9a-fA-F]{1,64}$/.test(signature.s) ||
-        signature.v < 0 ||
-        signature.v > 3
-    ) {
+    if (!/^(0x)?[0-9a-fA-F]{130}$/.test(signature)) {
         throw new Error(`invalid signature: ${signature}`);
+    }
+    if (signature.startsWith("0x")) {
+        signature = signature.substr(2);
+    }
+    const r = `${signature.substr(0, 64)}`;
+    const s = `${signature.substr(64, 64)}`;
+    const v = Number.parseInt(signature.substr(128, 2), 16);
+    if (v < 0 || v > 3) {
+        throw new Error(`invalid recoveryParam v: ${v}`);
     }
     return secp256k1
         .recoverPubKey(
@@ -92,8 +97,8 @@ export const recoverEcdsa = (
                 .keyFromPrivate(message, "hex")
                 .getPrivate()
                 .toString(10),
-            signature,
-            signature.v
+            { r, s, v },
+            v
         )
         .encode("hex")
         .slice(2);
